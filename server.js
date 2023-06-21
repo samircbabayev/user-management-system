505493ac8761db479a4169d197a00b23d4c3644a
samircbabayev <samircbabayev@gmail.com>
2023-06-21 23:39:04 +0400

    loading assets

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const dotenv  = require('dotenv');
-const morgan  = require('morgan');
+const express           = require('express');
+const dotenv            = require('dotenv');
+const morgan            = require('morgan');
+const bodyparser        = require('body-parser'); 
+const path              = require('path');
 
 dotenv.config({
   path: 'config.env'
@@ -10,7 +12,21 @@ const app     = express();
 
 const PORT = process.env.PORT || 8080;
 
+// log requests
 app.use(morgan('tiny'));
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({extended: true}));
+
+// set view engine
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname, "views/ejs"));
+
+// load assets
+app.use('/css',express.static(path.resolve(__dirname, "assets/css")));
+app.use('/img',express.static(path.resolve(__dirname, "assets/img")));
+app.use('/js',express.static(path.resolve(__dirname, "assets/js")));
+// css/style.css
 
 app.get('/', (req, res) => {
   res.send("Home page");
